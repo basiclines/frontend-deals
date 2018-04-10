f01494543d415d57355404400cbb2ddb6fcb527d
basiclines <igonzaleznicolas@gmail.com>
2018-04-10 11:33:24 +0200

    Removed hard coded widths

--- src/app.js
+++ src/app.js
@@ -10,10 +10,10 @@ class App extends LEOElement {
 			<fd-dev-browsers data-shows-on="all,tools" title="Browsers for development" class="fd-block"></fd-dev-browsers>
 			<fd-frameworks data-shows-on="all,tools" title="Frameworks" class="fd-block"></fd-frameworks>
 			<fd-build-systems data-shows-on="all,tools" title="Build systems" class="fd-block"></fd-build-systems>
-			<fd-online-training data-shows-on="all,learn" title="Online training" class="fd-block"></fd-online-training>
-			<fd-articles data-shows-on="all,grow,community" title="Latest Articles" class="fd-block"></fd-articles>
+			<fd-online-training data-shows-on="all,learn" title="Online training" class="fd-block link-preview"></fd-online-training>
+			<fd-articles data-shows-on="all,grow,community" title="Latest Articles" class="fd-block link-preview"></fd-articles>
 			<fd-libraries data-shows-on="all,grow" title="New Libraries" class="fd-block"></fd-libraries>
-			<fd-conferences data-shows-on="all,community,grow" title="Upcoming Conferences" class="fd-block"></fd-conferences>
+			<fd-conferences data-shows-on="all,community,grow" title="Upcoming Conferences" class="fd-block link-preview"></fd-conferences>
 			<fd-tools data-shows-on="all,tools" title="Services" class="fd-block"></fd-tools>
 			<footer>Made by <a href="https://ismael.fyi" target="_blank">ismael.fyi</a></footer>
 		`
